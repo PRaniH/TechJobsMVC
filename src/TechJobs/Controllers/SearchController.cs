0825bc3aeb8efc3ed8792d17154e6da0839c3553
P. Rani Hooda <ranih1213@gmail.com>
2019-12-17 21:26:32 -0500

    Still working on displaying search results

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -14,8 +14,8 @@ namespace TechJobs.Controllers
         }
 
         // TODO #1 - Create a Results action method to process 
-        // search request and display results
-        public IActionResult Results (string searchType, string searchTerm)
+        // Search the request and display results
+        public IActionResult Results(string searchType, string searchTerm)
         {
             //Create a list to store queried results to be returned
             if (searchType.Equals("all"))
@@ -26,25 +26,30 @@ namespace TechJobs.Controllers
                 ViewBag.title = "Searched Jobs"; //update to use the search type and term
                 ViewBag.columns = ListController.columnChoices;
                 ViewBag.jobs = jobs;
-                return View("");
+                return View("Index");
+                //After looking up the search results via the JobData class, you'll need to pass them into the 
+                //Views/Search/Index.cshtml view. Note that this is not the default view for this action.
+                //You'll also need to pass ListController.columnChoices to the view, as is done in the Index method.
             }
             else
             {
-
                 //Use the searchTerm and the searchType and use the FindByColumnandValue method and save that to the list
                 List<Dictionary<string, string>> items = JobData.FindByColumnAndValue(searchType, searchTerm);
                 ViewBag.title = "Searched Jobs"; //update to use the search type and term
                 ViewBag.columns = ListController.columnChoices;
-                ViewBag.items = items;
-                return View("");
-
+                ViewBag.jobs = items; //Is this needed?
+                ViewBag.items = items; 
+                return View("Index");
+                //After looking up the search results via the JobData class, you'll need to pass them into the 
+                //Views/Search/Index.cshtml view. Note that this is not the default view for this action.
+                //You'll also need to pass ListController.columnChoices to the view, as is done in the Index method.
 
             }
 
-            //set the value of Viewbag.columns property to the columnChoices of the ListController class
-            //then set the Viewbag.jobs property to the list
-            //then return the index view
-        } 
+
+
+
+        }
 
 
     }
