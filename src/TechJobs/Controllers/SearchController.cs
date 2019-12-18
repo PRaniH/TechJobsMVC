73a5433997adb828b5eb1a6c2e5b05f26cd6d654
P. Rani Hooda <ranih1213@gmail.com>
2019-12-17 19:38:45 -0500

    Revert

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -14,7 +14,7 @@ namespace TechJobs.Controllers
         }
 
         // TODO #1 - Create a Results action method to process 
-        // Search the request and display results
+        // search request and display results
         public IActionResult Results (string searchType, string searchTerm)
         {
             //Create a list to store queried results to be returned
@@ -26,28 +26,24 @@ namespace TechJobs.Controllers
                 ViewBag.title = "Searched Jobs"; //update to use the search type and term
                 ViewBag.columns = ListController.columnChoices;
                 ViewBag.jobs = jobs;
-                return View("Index"); 
-                //After looking up the search results via the JobData class, you'll need to pass them into the 
-                //Views/Search/Index.cshtml view. Note that this is not the default view for this action.
-                //You'll also need to pass ListController.columnChoices to the view, as is done in the Index method.
+                return View("");
             }
             else
             {
+
                 //Use the searchTerm and the searchType and use the FindByColumnandValue method and save that to the list
                 List<Dictionary<string, string>> items = JobData.FindByColumnAndValue(searchType, searchTerm);
                 ViewBag.title = "Searched Jobs"; //update to use the search type and term
                 ViewBag.columns = ListController.columnChoices;
-                ViewBag.items = items; //ViewBag.jobs = items;
-                return View("Index");
-                //After looking up the search results via the JobData class, you'll need to pass them into the 
-                //Views/Search/Index.cshtml view. Note that this is not the default view for this action.
-                //You'll also need to pass ListController.columnChoices to the view, as is done in the Index method.
+                ViewBag.items = items;
+                return View("");
+
 
             }
 
- 
-
-
+            //set the value of Viewbag.columns property to the columnChoices of the ListController class
+            //then set the Viewbag.jobs property to the list
+            //then return the index view
         } 
 
 
